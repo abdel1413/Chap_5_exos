8f286fb021ecfd7cf69197f77bef85291d14ac28
Your Name <you@example.com>
2022-08-21 22:22:13 -0400

    another testing function

--- Every.js
+++ Every.js
@@ -13,3 +13,14 @@ function every(array, predicate) {
 }
 
 console.log(every([1, 3, 5], (n) => n < 10));
+
+function every2(array, testingFunction) {
+  return !array.some((element) => !testingFunction(element));
+}
+
+console.log(every2([1, 3, 5], (n) => n < 10)); //true bcz all elememts < 10
+
+// → true
+console.log(every([2, 4, 16], (n) => n < 10));
+// → false   bcz 10 !< 10
+console.log(every([], (n) => n < 10)); //true
